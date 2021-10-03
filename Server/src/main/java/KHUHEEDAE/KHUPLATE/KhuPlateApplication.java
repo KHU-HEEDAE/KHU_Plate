e082a4ea6fd903cadd21060ff6c36a939408296a
Seoyeon Kim <2018103585@khu.ac.kr>
2021-10-03 20:39:38 +0900

    initial setting

---
+++ Server/src/main/java/KHUHEEDAE/KHUPLATE/KhuPlateApplication.java
@@ -0,0 +1,14 @@
+package KHUHEEDAE.KHUPLATE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuPlateApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KhuPlateApplication.class, args);
+	}
+
+}
