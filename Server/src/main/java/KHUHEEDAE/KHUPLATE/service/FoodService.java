f6827df5fa0bc7b77e523923c16115db934b1737
Seoyeon Kim <2018103585@khu.ac.kr>
2021-10-09 23:04:29 +0900

    음식점 조회 api 구현

---
+++ Server/src/main/java/KHUHEEDAE/KHUPLATE/service/FoodService.java
@@ -0,0 +1,39 @@
+package KHUHEEDAE.KHUPLATE.service;
+
+import KHUHEEDAE.KHUPLATE.domain.Food;
+import KHUHEEDAE.KHUPLATE.repository.CategoryRepository;
+import KHUHEEDAE.KHUPLATE.repository.FoodRepository;
+import KHUHEEDAE.KHUPLATE.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class FoodService {
+    private final FoodRepository foodRepository;
+    private final ReviewRepository reviewRepository;
+    private final CategoryRepository categoryRepository;
+
+    public FoodService(FoodRepository foodRepository, ReviewRepository reviewRepository, CategoryRepository categoryRepository) {
+        this.foodRepository = foodRepository;
+        this.reviewRepository = reviewRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Food> findAll(){
+        return foodRepository.findAll();
+    }
+
+    public Food findOne(Long foodId){
+        Optional<Food> food = foodRepository.findById(foodId);
+        return food.get();
+    }
+
+    public List<Food> findAllByCategory(Long categoryId){
+        return foodRepository.findAllByCategoryId(categoryId);
+    }
+
+}
