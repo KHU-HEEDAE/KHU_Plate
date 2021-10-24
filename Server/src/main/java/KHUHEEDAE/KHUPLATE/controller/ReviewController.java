6cc6bad5ebe38408782cb240f366ff39146c470d
Seoyeon Kim <2018103585@khu.ac.kr>
2021-10-24 23:25:42 +0900

    add Review CRUD

--- Server/src/main/java/KHUHEEDAE/KHUPLATE/controller/ReviewController.java
+++ Server/src/main/java/KHUHEEDAE/KHUPLATE/controller/ReviewController.java
@@ -4,9 +4,7 @@ import KHUHEEDAE.KHUPLATE.domain.Review;
 import KHUHEEDAE.KHUPLATE.dto.FoodDTO;
 import KHUHEEDAE.KHUPLATE.dto.ReviewDTO;
 import KHUHEEDAE.KHUPLATE.service.ReviewService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +21,7 @@ public class ReviewController {
     }
 
     //모든 리뷰 조회
-    @GetMapping("review")
+    @GetMapping("/review")
     public List<ReviewDTO> viewReviewList(){
         List<Review> reviews = reviewService.findAll();
         List<ReviewDTO> result = reviews.stream()
@@ -33,4 +31,24 @@ public class ReviewController {
         return result;
     }
 
+    //리뷰 추가
+    @PostMapping("/review")
+    public String writeReview(@RequestBody ReviewDTO request){
+        reviewService.write(request.toEntity());
+        return "리뷰 추가";
+    }
+
+    //리뷰 삭제
+    @DeleteMapping("/review")
+    public String deleteReview(@RequestParam("id") Long id){
+        reviewService.delete(id);
+        return "리뷰 삭제";
+    }
+
+    //리뷰 수정
+    @PatchMapping("/review")
+    public String updateReview(@RequestParam("id") Long id, @RequestBody ReviewDTO request){
+        reviewService.update(request.getContent(),request.getRate(),id);
+        return "리뷰 수정";
+    }
 }
--- Server/src/main/java/KHUHEEDAE/KHUPLATE/repository/ReviewRepository.java
+++ Server/src/main/java/KHUHEEDAE/KHUPLATE/repository/ReviewRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     List<Review> findAllByFoodId();
+
 }
